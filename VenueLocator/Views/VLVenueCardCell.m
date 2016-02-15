55b807dcb05dfa77358680467b5c07ce35913cd1
Tamas Nemeth <tm@autobutler.dk>
2016-02-15 15:28:18 +0100

    UPD - UI polish

--- VenueLocator/Views/VLVenueCardCell.m
+++ VenueLocator/Views/VLVenueCardCell.m
@@ -8,6 +8,38 @@
 
 #import "VLVenueCardCell.h"
 
+@import CoreImage;
+
 @implementation VLVenueCardCell
+
+- (instancetype) init {
+    self = [super init];
+    if (self) {
+        [self setup];
+    }
+    return self;
+}
+
+- (instancetype)initWithCoder:(NSCoder *)aDecoder {
+    self = [super initWithCoder:aDecoder];
+    if (self) {
+        [self setup];
+    }
+    return self;
+}
+
+- (instancetype)initWithFrame:(CGRect)frame {
+    self = [super initWithFrame:frame];
+    if (self) {
+        [self setup];
+    }
+    return self;
+}
+
+- (void)setup {
+    self.layer.cornerRadius = 8.0f;
+    self.layer.borderColor = [UIColor darkGrayColor].CGColor;
+    self.layer.borderWidth = 0.5f;
+}
 
 @end
