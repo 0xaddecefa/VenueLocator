9c82733498404cdd20ef1b1166a6cbf687fc87f0
Tamas Nemeth <tm@autobutler.dk>
2016-02-16 11:30:54 +0100

    Unit test case - SearchPresenter's successful search use case

--- VenueLocatorTests/Mocks/VLMockSearchSource.m
+++ VenueLocatorTests/Mocks/VLMockSearchSource.m
@@ -17,8 +17,24 @@
        successCallback: (void (^ __nullable)(VLVenueList * _Nullable))successCallback
          errorCallback: (void (^ __nullable)(NSError  * _Nullable ))errorCallback {
     
-    
-    
+    if (successCallback) {
+        //this could go into a plist
+        NSDictionary *validInput = @{ @"venues" :
+                                          @[@{
+                                                @"id" : @"1234",
+                                                @"name" : @"test",
+                                                @"location" : @{
+                                                        @"distance" : @(12),
+                                                        @"formattedAddress" : @[@"Address 1", @"Address 2"]
+                                                        }
+                                                },
+                                            ]
+                                      };
+        
+        VLVenueList *venueList = [[VLVenueList alloc] initWithDictionary:validInput];
+        
+        successCallback(venueList);
+    }
 }
 
 @end
